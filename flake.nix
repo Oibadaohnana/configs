fbfa2dcdbce1a5d6e68b1c4b655df75f14df04e3
Toni Peter <mail@tonipeter.de>
2025-08-31 15:09:40 +0200

    foo

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "My NixOS config using flakes with Hyprland";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: {
+    nixosConfigurations."benji" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
