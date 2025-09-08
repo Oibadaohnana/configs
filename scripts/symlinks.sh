43e491636231a279b90f730d8627270641e485a9
benji <bennywuest@gmail.com>
2025-09-08 10:44:51 +0200

    update2

---
+++ scripts/symlinks.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [[ -z $HOME ]]; then
+    echo "Environment variable HOME not set."
+    exit 2
+fi
+
+CONFIG=$HOME/configs
+
+function make_symlink {
+    target="$CONFIG/$1"
+    name="$HOME/$2"
+    mkdir -p $(dirname "$name")
+    if [[ -L "$name" ]]; then
+        if [[ $(readlink -f "$name") != $target ]]; then
+            echo "Deleting previously existing symlink: $target"
+            rm $name
+            ln -s "$target" "$name"
+        fi
+    elif [[ ! -a "$name" ]]; then
+        echo "$target\t\t -> $name"
+        ln -s "$target" "$name"
+    else
+        echo "File already exists: $name"
+    fi
+}
+
+make_symlink configs/bashrc .bashrc
