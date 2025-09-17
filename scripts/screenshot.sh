15f4a20489b2727b57281b2dd192173ddcbc4ea2
Oibadaohnana <bennywuest@gmail.com>
2025-09-17 13:27:27 +0200

    update 7 generall improvements

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 grim -g "$(slurp)" - | tee /tmp/screenshot.png | wl-copy
-notify-send "📸 Screenshot copied to clipboard"
+notify-send "📸 Screenshot copied to clipboard you awesome stupid biaatch"
