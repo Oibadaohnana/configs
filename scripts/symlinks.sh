cef581900017e59568fed377719f1ee9bc7c59c1
Oibadaohnana <bennywuest@gmail.com>
2025-09-23 10:37:08 +0200

    Adjustments and Hyprland config (not setup)

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -30,5 +30,6 @@ make_symlink configs/swayconfig .config/sway/config
 make_symlink configs/waybarconfig/config .config/waybar/config
 make_symlink configs/waybarconfig/style.css .config/waybar/style.css
 make_symlink configs/makoconfig .config/mako/config
+make_symlink configs/hyprland .config/hypr/hyprland
 
 
