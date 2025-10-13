6418819b60053d8a9c8bf60231cbaed137a907e4
Oibadaohnana <bennywuest@gmail.com>
2025-10-13 18:56:53 +0200

    what mic addition

--- scripts/mic_mute_state_notify.sh
+++ scripts/mic_mute_state_notify.sh
@@ -6,8 +6,13 @@ pactl set-source-mute $source toggle
 
 MUTE_state=$(pactl get-source-mute $source | awk '{print $2}')
 
+mic_name=$(pactl list sources | awk -v src="$source" '
+    $0 ~ "Name: "src {found=1}
+    found && /Description:/ {print substr($0, index($0,$2)); exit}
+')
+
 if [[ "$MUTE_state" == "yes" ]]; then
-    notify-send "🎤🔇 Mic is muted"
+    notify-send "🎤🔇 Mic is muted" "$mic_name"
 else
-    notify-send "🎤🔊 Mic is unmuted"
+    notify-send "🎤🔊 Mic is unmuted" "$mic_name"
 fi
